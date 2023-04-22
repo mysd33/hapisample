877ccb2f1629179191c43b3c17287f67929483fe
mysd33 <mysd.33@gmail.com>
2023-04-23 07:42:30 +0900

    患者情報の取得の実装例追加

--- src/main/java/hapisample/Main.java
+++ src/main/java/hapisample/Main.java
@@ -1,5 +1,6 @@
 package hapisample;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -9,6 +10,8 @@ import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.Composition;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ResourceType;
@@ -24,23 +27,23 @@ import ca.uhn.fhir.validation.ValidationResult;
 
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
-
+    
+    // （参考）
+    // https://hapifhir.io/hapi-fhir/docs/model/parsers.html
+    // https://hapifhir.io/hapi-fhir/docs/validation/instance_validator.html
     public static void main(String[] args) throws FileNotFoundException {
+        // あえて分かりやすくするため１つのメソッドに手続き的に書いてあるので、本当に実装したい場合は保守性の高いモジュール化されたコード書くこと
+        
         // 診療情報提供書のHL7 FHIRのサンプルデータを読み込み
-        InputStream is = new FileInputStream("file/input/Bundle-BundleReferralExample01.json");
-
+        InputStream is = new BufferedInputStream(new FileInputStream("file/input/Bundle-BundleReferralExample01.json"));
         // FHIRコンテキスト作成
         FhirContext ctx = FhirContext.forR4();
-
-        // HAPI FHIRを使ってパース
-        // https://hapifhir.io/hapi-fhir/docs/model/parsers.html
         // パーサを作成
         IParser parser = ctx.newJsonParser();
         // サンプルデータをパースしBundleリソースを取得
         Bundle bundle = parser.parseResource(Bundle.class, is);
 
-        // HAPI FHIRを使ってFHIR公式の検証ルールに基づいているか検証
-        // https://hapifhir.io/hapi-fhir/docs/validation/instance_validator.html
+        // FHIR公式の検証ルールに基づいているか検証    
         // Validatorの作成
         FhirValidator validator = ctx.newValidator();
         IValidatorModule module = new FhirInstanceValidator(ctx);
@@ -48,41 +51,60 @@ public class Main {
         // 検証
         ValidationResult validationResult = validator.validateWithResult(bundle);
         if (!validationResult.isSuccessful()) {
-            for (SingleValidationMessage validationMessage : validationResult.getMessages()) {
+            // 検証結果の出力
+            for (SingleValidationMessage validationMessage : validationResult.getMessages()) {                
                 logger.warn("[{}] {}", validationMessage.getLocationString(), validationMessage.getMessage());
             }
         }
-
+        
         // Bundleリソースを解析
         logger.info("Bundle type:{}", bundle.getType().getDisplay());
-
         // BundleからEntityを取得
         List<BundleEntryComponent> entities = bundle.getEntry();
-
+        String subjectRefId = null;
         // Entity内のResourceを取得
-        entities.forEach(e -> {
-            Resource resource = e.getResource();
+        for (BundleEntryComponent entity : entities) {
+            Resource resource = entity.getResource();
             ResourceType resourceType = resource.getResourceType();
             logger.info("Resource Type: {}", resourceType.name());
-
             switch (resourceType) {
             case Composition:
                 // Compositionリソースを解析する例
                 Composition composition = (Composition) resource;
                 String title = composition.getTitle();
                 logger.info("文書名: {}", title);
-
+                // subjectの参照先のUUIDを取得
                 Reference subjectRef = composition.getSubject();
-                logger.info("subject reference: {}", subjectRef.getReference());
+                subjectRefId = subjectRef.getReference();
                 logger.info("subject display: {}", subjectRef.getDisplay());
-                // TODO: 処理の追加
+                logger.info("subject reference Id: {}", subjectRefId);
+                // TODO: 各参照先のUUIDを取得する処理の追加
+                break;
+            case Patient:
+                // Patientリソースを解析する例
+                if (!entity.getFullUrl().equals(subjectRefId)) {
+                    break;
+                }
+                logger.info("Composition.subjectの参照先のPatient:{}", subjectRefId);
+                Patient patient = (Patient) resource;
+                // 患者番号の取得
+                logger.info("患者番号:{}", patient.getIdentifier().get(0).getValue());
+                // 患者氏名の取得
+                List<HumanName> humanNames = patient.getName();                
+                humanNames.forEach(humanName -> {
+                   String valueCode = humanName.getExtensionString("http://hl7.org/fhir/StructureDefinition/iso21090-EN-representation");
+                   if ("IDE".equals(valueCode)) {
+                       logger.info("患者氏名:{}",humanName.getText());
+                   } else {
+                       logger.info("患者カナ氏名:{}",humanName.getText());
+                   }
+                });                
                 break;
             // TODO: リソース毎に処理の追加
             default:
                 break;
             }
-
-        });
+        }
 
     }
 }
